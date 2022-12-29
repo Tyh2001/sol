313c2167aed75938ae97036cb77ce924eff0c2f7
tyh2001 <1469442737@qq.com>
2022-12-29 08:59:46 +0800

    fest: 初始化

---
+++ 1.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.11;
+
+contract helloWorld {
+  function render() public {
+    return 'hello world'
+  }
+}
+
+// contract helloWorld {
+//   function renderHelloWorld() returns (string) {
+//     return 'helloWorld';
+//   };
+// }
+
---
+++ README.md
@@ -0,0 +1,19 @@
+# 编写智能合约
+
+## solidity 开发环境搭建
+
+1. 安装 [solc](https://www.npmjs.com/package/solc)
+
+```shell
+npm install -g solc
+```
+
+查看安装的版本
+
+```shell
+solcjs --version
+```
+
+2. 安装 Ganache
+
+在 [官网](https://trufflesuite.com/ganache/) 安装
